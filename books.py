fa0ebab928e839eed1a449ee2232f9aff76aef54
Jose Michael Rubio <contact@josemichaelrubio.com>
2023-09-14 14:49:48 -0400

    added dynamic path

--- books.py
+++ books.py
@@ -15,3 +15,13 @@ BOOKS = [
 @app.get('/books')
 async def read_all_books():
     return BOOKS
+
+
+@app.get('/books/{book_title}')
+async def read_all_books(book_title: str):
+    for book in BOOKS:
+        if book.get('title').casefold() == book_title.casefold():
+            return book
+
+
+
