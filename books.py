199f818ebaa532329518c634b47d2179544643d5
Jose Michael Rubio <contact@josemichaelrubio.com>
2023-09-15 12:47:22 -0400

    added put request

--- books.py
+++ books.py
@@ -46,3 +46,10 @@ async def read_author_genre_by_query(book_author: str, genre: str):
 @app.post('/books/create_book')
 async def create_book(new_book=Body()):
     BOOKS.append(new_book)
+
+
+@app.put('/books/update_book')
+async def update_book(update_book=Body()):
+    for i in range(len(BOOKS)):
+        if BOOKS[i].get('title').casefold() == update_book.get('title').casefold():
+            BOOKS[i] = update_book
