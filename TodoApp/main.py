6cc77362a103bbdd3a5158d791164bc8dc85afef
Jose Michael Rubio <contact@josemichaelrubio.com>
2023-09-25 10:04:01 -0400

    Added Put request to update a todo based on id

--- TodoApp/main.py
+++ TodoApp/main.py
@@ -55,3 +55,17 @@ async def create_todo(db: db_dependency, todo_request: TodoRequest):
     todo_model = ToDos(**todo_request.model_dump())
     db.add(todo_model)
     db.commit()
+
+
+@app.put("/todo/{todo_id}", status_code=status.HTTP_202_ACCEPTED)
+async def update_todo(db: db_dependency, todo_request: TodoRequest, todo_id: int = Path(gt=0)):
+    todo_model = db.query(ToDos).filter(ToDos.id == todo_id).first()
+    if todo_model is None:
+        raise HTTPException(status_code=404, detail=f'Todo with id {todo_id} does not exist')
+    todo_model.title = todo_request.title
+    todo_model.description = todo_request.description
+    todo_model.priority = todo_request.priority
+    todo_model.complete = todo_request.complete
+    db.add(todo_model)
+    db.commit()
+
