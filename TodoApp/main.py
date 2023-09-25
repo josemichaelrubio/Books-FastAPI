674d6cc4fed5ffdd3f5a0b050cf73cba8d4464c8
Jose Michael Rubio <contact@josemichaelrubio.com>
2023-09-25 08:25:28 -0400

    added api endpoint to get specific id. With imports and usage of status, Path, and HTTPException

--- TodoApp/main.py
+++ TodoApp/main.py
@@ -1,7 +1,8 @@
 from typing import Annotated
 
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Depends, HTTPException, Path
 from sqlalchemy.orm import Session
+from starlette import status
 
 import models
 from models import ToDos
@@ -25,6 +26,13 @@ def get_db():
 db_dependency = Annotated[Session, Depends(get_db)]
 
 
-@app.get('/')
+@app.get('/', status_code=status.HTTP_200_OK)
 async def read_all(db: db_dependency):
     return db.query(ToDos).all()
+
+@app.get('/todo/{todo_id}', status_code=status.HTTP_200_OK)
+async def read_one(db: db_dependency, todo_id: int = Path(gt=0)):
+    todo_model = db.query(ToDos).filter(ToDos.id == todo_id).first()
+    if todo_model is not None:
+        return todo_model
+    raise HTTPException(status_code=404, detail=f'Todo with id {todo_id} does not exist')
