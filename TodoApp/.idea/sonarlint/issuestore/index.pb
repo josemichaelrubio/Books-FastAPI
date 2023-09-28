496bca0fda997c6eb588e08b0cd8dd1168cc474c
Jose Michael Rubio <contact@josemichaelrubio.com>
2023-09-28 08:39:21 -0400

    Can now create users into database

---
+++ TodoApp/.idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,11 @@
+
+7
+main.py,f/1/f1bdda93d9a278e358509d498e17d97764c1fb29
+;
+database.py,1/2/12d0bfc8adac6eee15262b728a538821e35c98c9
+@
+routers/todos.py,5/c/5c8d53d7b703e51a132123364d9a201d9243c2bc
+?
+routers/auth.py,8/5/85f203a072ecaeff13fda28035aae708563828ea
+9
+	models.py,8/5/85f64e80ba345158acc186d4505f039d4f21420f
--- TodoApp/routers/auth.py
+++ TodoApp/routers/auth.py
@@ -1,11 +1,17 @@
-from fastapi import APIRouter
+from typing import Annotated
+from fastapi import APIRouter, Depends
 from pydantic import BaseModel
+from sqlalchemy.orm import Session
+from starlette import status
+
+from database import SessionLocal
 from models import Users
 from passlib.context import CryptContext
 
 router = APIRouter()
 
 bcrypt_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
 
 class CreateUserRequest(BaseModel):
     username: str
@@ -18,8 +24,21 @@ class CreateUserRequest(BaseModel):
     # leaving out is_active
 
 
-@router.post("/auth")
-async def create_user(create_user_request: CreateUserRequest):
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+# Opens up db connection as a dependency injection
+db_dependency = Annotated[Session, Depends(get_db)]
+
+
+@router.post("/auth", status_code=status.HTTP_201_CREATED)
+async def create_user(db: db_dependency,
+        create_user_request: CreateUserRequest):
     create_user_model = Users(
         username=create_user_request.username,
         email=create_user_request.email,
@@ -29,4 +48,7 @@ async def create_user(create_user_request: CreateUserRequest):
         role=create_user_request.role,
         is_active=True
     )
-    return create_user_model
+    db.add(create_user_model)
+    db.commit()
+
+@router.post("/token")
