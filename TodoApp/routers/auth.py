a907371e581d6bd51d15d7ac9631bd15aec9c6b2
Jose Michael Rubio <contact@josemichaelrubio.com>
2023-09-29 12:17:43 -0400

    Authenication enhancements

--- TodoApp/routers/auth.py
+++ TodoApp/routers/auth.py
@@ -11,13 +11,16 @@ from passlib.context import CryptContext
 from fastapi.security import OAuth2PasswordRequestForm, OAuth2PasswordBearer
 from jose import jwt, JWTError
 
-router = APIRouter()
+router = APIRouter(
+    prefix='/auth',
+    tags=['auth']
+)
 
 SECRET_KEY = '59b320002b11581c550a250b771a6ed7763314c563bcc5f5db615a206fff3ad8'
 ALGORITHM = 'HS256'
 
 bcrypt_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
-oauth2_bearer= OAuth2PasswordBearer(tokenUrl='token')
+oauth2_bearer = OAuth2PasswordBearer(tokenUrl='auth/token')
 
 
 class CreateUserRequest(BaseModel):
@@ -30,9 +33,11 @@ class CreateUserRequest(BaseModel):
     # Leaving out id
     # leaving out is_active
 
+
 class Token(BaseModel):
     access_token: str
     token_type: str
+
 
 def get_db():
     db = SessionLocal()
@@ -61,6 +66,7 @@ def create_access_token(username: str, user_id: int, expires_delta: timedelta):
     encode.update({'exp': expires})
     return jwt.encode(encode, SECRET_KEY, algorithm=ALGORITHM)
 
+
 # verifies user
 async def get_current_user(token: Annotated[str, Depends(oauth2_bearer)]):
     try:
@@ -75,7 +81,8 @@ async def get_current_user(token: Annotated[str, Depends(oauth2_bearer)]):
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,
                             detail='Could not validate credentials.')
 
-@router.post("/auth", status_code=status.HTTP_201_CREATED)
+
+@router.post("/", status_code=status.HTTP_201_CREATED)
 async def create_user(db: db_dependency,
                       create_user_request: CreateUserRequest):
     create_user_model = Users(
@@ -96,6 +103,7 @@ async def login_for_access_token(form_data: Annotated[OAuth2PasswordRequestForm,
                                  db: db_dependency):
     user = authenticate_user(form_data.username, form_data.password, db)
     if not user:
-        return 'Failed Authentication'
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,
+                            detail='Could not validate credentials.')
     token = create_access_token(user.username, user.id, timedelta(minutes=20))
-    return { 'access_token': token, 'token_type': 'bearer'}
+    return {'access_token': token, 'token_type': 'bearer'}
