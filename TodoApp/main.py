c37f7cf1d3ddcfb6e5a53e5f218f584f272775ad
Jose Michael Rubio <contact@josemichaelrubio.com>
2023-11-01 12:47:31 -0400

    Added css and js files to ensure bootstrap is working properly

--- TodoApp/main.py
+++ TodoApp/main.py
@@ -2,10 +2,13 @@ from fastapi import FastAPI
 import models
 from database import engine
 from routers import auth, todos, admin, users
+from starlette.staticfiles import StaticFiles
 
 app = FastAPI()
 
 models.Base.metadata.create_all(bind=engine)
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
 app.include_router(auth.router)
 app.include_router(todos.router)
