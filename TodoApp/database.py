9b33a1cf55e13d2840bc9b3b2e74410ca10e7f10
Jose Michael Rubio <contact@josemichaelrubio.com>
2024-09-05 13:21:08 -0400

    Update database.py

--- TodoApp/database.py
+++ TodoApp/database.py
@@ -1,5 +1,15 @@
-p
 
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+
+SQLALCHEMY_DATABASE_URI = 'sqlite:///./todosapp.db'
+
+engine = create_engine(SQLALCHEMY_DATABASE_URI, connect_args={'check_same_thread': False})
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
 
 # import os
 # from fastapi import FastAPI, HTTPException
